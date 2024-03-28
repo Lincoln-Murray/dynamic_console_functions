30eb2a5f11b547546e9892c1dad98d959bc4fdd8
Lincoln Murray <134500266+Lincoln-Murray@users.noreply.github.com>
2024-03-28 14:55:33 +0800

    Update Main.py
    Added user input validation, give it a list and it will validate the input until it has one of the correct inputs, will return the index of the _option_list that was chosen.

--- Main.py
+++ Main.py
@@ -1,1 +1,21 @@
 
+def user_input(_option_list:list, _input_text:str = "What would you like to do? ") -> int:
+    valid = False
+    print(option_list)
+    while not valid:
+        user_input = input()
+        if user_input in _option_list:
+            option = _option_list.index(user_input)
+            valid = True
+            return option
+        else:
+            try:
+                user_input = int(user_input)
+                if user_input < len(option_list) and user_input > len(option_list):
+                    valid = True
+                    return user_input
+                else:
+                    raise TypeError
+            except:
+                valid = False
+                print('Invalid selection, please try again.')
