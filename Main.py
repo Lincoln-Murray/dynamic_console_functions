322255de7d23bb9037f092d2d5d47b7f2b22f79b
Lincoln Murray <134500266+Lincoln-Murray@users.noreply.github.com>
2024-03-29 08:11:35 +0800

    Update Main.py
    Fixed bugs

--- Main.py
+++ Main.py
@@ -1,17 +1,16 @@
 
 def user_input(_option_list:list, _input_text:str = "What would you like to do? ") -> int:
     valid = False
-    print(option_list)
     while not valid:
-        user_input = input()
+        user_input = input(_input_text)
         if user_input in _option_list:
             option = _option_list.index(user_input)
             valid = True
             return option
         else:
             try:
-                user_input = int(user_input)
-                if user_input < len(option_list) and user_input > len(option_list):
+                user_input = int(user_input)-1
+                if user_input <= len(_option_list)-1 and user_input >= 0:
                     valid = True
                     return user_input
                 else:
