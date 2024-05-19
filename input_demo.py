1984343df202a69db89bd39ee617110516fa72bf
Lincoln-Murray <134500266+Lincoln-Murray@users.noreply.github.com>
2024-05-19 15:24:14 +0800

    Update input_demo.py
    Made demo nicer.

--- input_demo.py
+++ input_demo.py
@@ -1,6 +1,8 @@
 import Main
 
+print('Choosing from a table: ')
 ol = ['a', 'b', 'c']
 print(ol[Main.user_input(ol)])
 
+print('\n choosing a number in a range: ')
 print(Main.number_input(float, '??', 0.5, 10))
